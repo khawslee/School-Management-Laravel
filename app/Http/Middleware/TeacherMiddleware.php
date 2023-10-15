908efb8707fcd9e9792b459a90dbe481a807773a
Siang Lee, Khaw <khawslee@gmail.com>
2023-10-15 14:42:30 +0800

    Middleware

---
+++ app/Http/Middleware/TeacherMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class TeacherMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(Auth::check() && Auth::user()->user_type == 2) {
+            return $next($request);
+        }
+        else
+        {
+            Auth::logout();
+            return redirect((url('')));
+        }
+    }
+}
