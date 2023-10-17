688ea3ab1b811002060571fc80e89ddf5d647677
Siang Lee, Khaw <khawslee@gmail.com>
2023-10-17 19:39:05 +0800

    Add middleware in Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,10 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'teacher' => \App\Http\Middleware\TeacherMiddleware::class,
+        'student' => \App\Http\Middleware\StudentMiddleware::class,
+        'parent' => \App\Http\Middleware\ParentMiddleware::class,
     ];
 }
