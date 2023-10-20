3cd2c65b3d80d0da37a9b9963d9fc29d42c841b9
Siang Lee, Khaw <khawslee@gmail.com>
2023-10-20 10:53:02 +0800

    user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'user_type',
+        'is_delete',
     ];
 
     /**
@@ -42,4 +44,28 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    static public function getEmailSingle($email)
+    {
+        return User::where('email', '=', $email)->first();
+    }
+
+    static public function getTokenSingle($remember_token)
+    {
+        return User::where('remember_token', '=', $remember_token)->first();
+    }
+
+    static public function getAdmin()
+    {
+        return self::select('users.*')
+            ->where('user_type', '=', 1)
+            ->where('is_delete', '=', 0)
+            ->orderBy('id', 'desc')
+            ->get();
+    }
+
+    static public function getSingle($id)
+    {
+        return self::find($id);
+    }
 }
