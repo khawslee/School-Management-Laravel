c570054b4702cb70f1e41b452d9af1ae5e0a0c76
Siang Lee, Khaw <khawslee@gmail.com>
2023-10-25 18:10:14 +0800

    Add ClassController

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\ClassModel;
 
 class ClassController extends Controller
 {
     public function list()
     {
-        return view('admin.class.list');
+        $data['getRecords'] = ClassModel::getRecords();
+        $data['header_title'] = 'Class List';
+        return view('admin.class.list', $data);
     }
+
+    public function add()
+    {
+        $data['header_title'] = 'Add New Class';
+        return view('admin.class.add', $data);
+    }
+
+    public function insert(Request $request)
+    {
+        $user= new ClassModel();
+        $user->name = trim($request->name);
+        $user->status = trim($request->status);
+        $user->create_by = Auth::user()->id;
+        $user->save();
+
+        return redirect('admin/class/list')->with('success', 'Class added successfully.');
+    }
+
 }
