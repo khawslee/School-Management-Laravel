c374335824ae1c88caf435a78710196d0094f5c5
Siang Lee, Khaw <khawslee@gmail.com>
2023-10-26 10:22:37 +0800

    Update web route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ClassController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -37,7 +38,12 @@ Route::group(['middleware' => 'admin'], function () {
     Route::post('admin/admin/edit/{id}', [AdminController::class, 'update']);
     Route::get('admin/admin/delete/{id}', [AdminController::class, 'delete']);
 
-    Route::get('admin/class/list', [ClassnController::class, 'list']);
+    Route::get('admin/class/list', [ClassController::class, 'list']);
+    Route::get('admin/class/add', [ClassController::class, 'add']);
+    Route::post('admin/class/add', [ClassController::class, 'insert']);
+    Route::get('admin/class/edit/{id}', [ClassController::class, 'edit']);
+    Route::post('admin/class/edit/{id}', [ClassController::class, 'update']);
+    Route::get('admin/class/delete/{id}', [ClassController::class, 'delete']);
 });
 
 Route::group(['middleware' => 'teacher'], function () {
