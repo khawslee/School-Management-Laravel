b0a4f30c0da0ac9033dd109a7c097802ea3ec16c
Siang Lee, Khaw <khawslee@gmail.com>
2023-11-01 12:36:15 +0800

    Assign Subject router

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ClassController;
+use App\Http\Controllers\ClassSubjectController;
+use App\Http\Controllers\SubjectController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,6 +34,7 @@ Route::post('reset/{token}', [AuthController::class, 'postReset']);
 Route::group(['middleware' => 'admin'], function () {
 
     Route::get('admin/dashboard', [DashboardController::class, 'dashboard']);
+
     Route::get('admin/admin/list', [AdminController::class, 'list']);
     Route::get('admin/admin/add', [AdminController::class, 'add']);
     Route::post('admin/admin/add', [AdminController::class, 'insert']);
@@ -44,6 +48,20 @@ Route::group(['middleware' => 'admin'], function () {
     Route::get('admin/class/edit/{id}', [ClassController::class, 'edit']);
     Route::post('admin/class/edit/{id}', [ClassController::class, 'update']);
     Route::get('admin/class/delete/{id}', [ClassController::class, 'delete']);
+
+    Route::get('admin/subject/list', [SubjectController::class, 'list']);
+    Route::get('admin/subject/add', [SubjectController::class, 'add']);
+    Route::post('admin/subject/add', [SubjectController::class, 'insert']);
+    Route::get('admin/subject/edit/{id}', [SubjectController::class, 'edit']);
+    Route::post('admin/subject/edit/{id}', [SubjectController::class, 'update']);
+    Route::get('admin/subject/delete/{id}', [SubjectController::class, 'delete']);
+
+    Route::get('admin/assign_subject/list', [ClassSubjectController::class, 'list']);
+    Route::get('admin/assign_subject/add', [ClassSubjectController::class, 'add']);
+    Route::post('admin/assign_subject/add', [ClassSubjectController::class, 'insert']);
+    Route::get('admin/assign_subject/edit/{id}', [ClassSubjectController::class, 'edit']);
+    Route::post('admin/assign_subject/edit/{id}', [ClassSubjectController::class, 'update']);
+    Route::get('admin/assign_subject/delete/{id}', [ClassSubjectController::class, 'delete']);
 });
 
 Route::group(['middleware' => 'teacher'], function () {
