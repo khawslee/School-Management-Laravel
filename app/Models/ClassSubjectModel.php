8ed349dfa2ee00fcdd0660d64be044bdb67f3aa2
Siang Lee, Khaw <khawslee@gmail.com>
2023-11-05 12:09:19 +0800

    Update Class subject model

--- app/Models/ClassSubjectModel.php
+++ app/Models/ClassSubjectModel.php
@@ -14,12 +14,19 @@ class ClassSubjectModel extends Model
 
     static public function getRecords()
     {
-        $return = ClassSubjectModel::select('class_subject.*', 'users.name as created_by_name')
+        $return = ClassSubjectModel::select('class_subject.*', 'class.name as class_name', 'subject.name as subject_name', 'users.name as created_by_name')
+                    ->join('subject', 'subject.id', '=', 'class_subject.subject_id')
+                    ->join('class', 'class.id', '=', 'class_subject.class_id')
                     ->join('users', 'users.id', '=', 'class_subject.created_by');
 
-                    if(!empty(Request::get('name')))
+                    if(!empty(Request::get('class_name')))
                     {
-                        $return = $return->where('class_subject.name', 'like', '%'.Request::get('name').'%');
+                        $return = $return->where('class.name', 'like', '%'.Request::get('class_name').'%');
+                    }
+
+                    if(!empty(Request::get('subject_name')))
+                    {
+                        $return = $return->where('subject.name', 'like', '%'.Request::get('subject_name').'%');
                     }
 
                     if(!empty(Request::get('date')))
@@ -37,5 +44,20 @@ class ClassSubjectModel extends Model
     static public function getSingle($id)
     {
         return self::find($id);
+    }
+
+    static public function getAlreadyAssign($class_id, $subject_id)
+    {
+        return self::where('class_id', $class_id)->where('subject_id', $subject_id)->first();
+    }
+
+    static public function getAssignSubjectID($class_id)
+    {
+        return self::where('class_id', $class_id)->where('is_deleted', '0')->get();
+    }
+
+    static public function deleteSubject($class_id)
+    {
+        return self::where('class_id', $class_id)->delete();
     }
 }
