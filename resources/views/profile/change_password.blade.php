9704a31cbad81edb8e256b91263c1adfc589dc74
Siang Lee, Khaw <khawslee@gmail.com>
2023-11-09 15:15:44 +0800

    New change password blade

---
+++ resources/views/profile/change_password.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Change Password</h1>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <!-- left column -->
+                    @include('_message')
+
+                    <div class="col-md-12">
+                        <!-- general form elements -->
+                        <form method="post" action="">
+                            @csrf
+                           <div class="card card-primary">
+                                <!-- form start -->
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label>Old Password</label>
+                                        <input type="password" class="form-control" required name="old_password"
+                                            placeholder="Old Password">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>New Password</label>
+                                        <input type="password" class="form-control" required name="new_password"
+                                            placeholder="New Password">
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Update</button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!--/.col (left) -->
+
+            </div>
+            <!-- /.row -->
+    </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
